6d746fcd74b5a80d5d4a579498764ffb753105f2
Avijit <avijit@cognitiveview.com>
2023-05-16 13:34:51 +1000

    Pair Coding Challenge Commit - Avijit

--- index.js
+++ index.js
@@ -1,11 +1,76 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
+const digitToName = {
+  0: "zero",
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  11: "eleven",
+  12: "twelve",
+  13: "thirteen",
+  14: "fourteen",
+  15: "quarter",
+  16: "sixteen",
+  17: "seventeen",
+  18: "eighteen",
+  19: "nineteen",
+  20: "twenty",
+  21: "twenty-one",
+  22: "twenty-two",
+  23: "twenty-three",
+  24: "twenty-four",
+  25: "twenty-five",
+  26: "twenty-six",
+  27: "twenty-seven",
+  28: "twenty-eight",
+  29: "twenty-nine",
+  30: "half"
+}
+
 function convertTimeToWords(time) {
   // TODO: real code goes here!
   if (time === '0:00') {
     return 'midnight';
   }
 
-  return 'half past eight';
+  let splitTime = time.split(':')
+  let firstHalf = parseInt(splitTime[0])
+  let secondHalf = parseInt(splitTime[1])
+  let stringTime = ""
+
+  if (firstHalf === 0 && secondHalf === 0){
+    return "midnight"
+  }
+  if (firstHalf === 12 && secondHalf === 0){
+    return "midday"
+  }
+  
+  let secondHalfString = String(digitToName[secondHalf]) + " past "
+  if (secondHalf > 30) {
+    secondHalfString = String(digitToName[60 - secondHalf]) + " to "
+    firstHalf = firstHalf + 1
+    if (firstHalf === 24){
+      firstHalf = 0
+    }
+  }
+  let fristHalfString = String(digitToName[firstHalf])
+  if (secondHalf === 0){
+    secondHalfString = ""
+    fristHalfString = fristHalfString + " o'clock"
+  }
+
+  if (firstHalf === 12){
+    fristHalfString = "midday"
+  }
+  
+  stringTime = secondHalfString + fristHalfString
+  return stringTime;
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -15,4 +15,34 @@ describe('Time to words', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
   });
+
+  it('Handles times at Midday - 11:45', () => {
+    const timeInWords = convertTimeToWords('11:45');
+    expect(timeInWords).toBe('quarter to midday');
+  });
+
+  it('Handles times at 29 minutes - 2:29', () => {
+    const timeInWords = convertTimeToWords('2:29');
+    expect(timeInWords).toBe('twenty-nine past two');
+  });
+
+  it('Handles times at 1 minutes to - 2:59', () => {
+    const timeInWords = convertTimeToWords('2:59');
+    expect(timeInWords).toBe('one to three');
+  });
+
+  it('Handles times at midday to - 12:00', () => {
+    const timeInWords = convertTimeToWords('12:00');
+    expect(timeInWords).toBe('midday');
+  });
+
+  it('Handles times at O clock - 2:00', () => {
+    const timeInWords = convertTimeToWords('2:00');
+    expect(timeInWords).toBe("two o'clock");
+  });
+
+  it('Handles times at 30 past - 2:30', () => {
+    const timeInWords = convertTimeToWords('2:30');
+    expect(timeInWords).toBe("half past two");
+  });
 });
